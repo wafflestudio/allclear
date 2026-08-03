cd6f46b6eba3fe528ec32f689db4bfbc3cce0432
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-08-03 22:55:53 +0900

    chore: configure Biome formatter

--- src/tabs/RegisterClubTab.tsx
+++ src/tabs/RegisterClubTab.tsx
@@ -6,10 +6,7 @@ const Stack = createNativeStackNavigator()
 export function RegisterClubTab() {
 	return (
 		<Stack.Navigator screenOptions={{ headerBackTitleVisible: false, headerShown: false }}>
-			<Stack.Screen
-				name="RegisterClub"
-				component={RegisterClubScreenContainer}
-			/>
+			<Stack.Screen name="RegisterClub" component={RegisterClubScreenContainer} />
 		</Stack.Navigator>
 	)
 }
